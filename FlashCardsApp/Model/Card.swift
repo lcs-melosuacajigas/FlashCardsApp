de2b8503a9b8642d91e99326123a2bdd0daca85d
Mateo Elosua <melosua@lcs.on.ca>
2021-12-07 14:28:28 -0500

    Added a few questions to the FlashCard structure

---
+++ FlashCardsApp/Model/Card.swift
@@ -0,0 +1,23 @@
+//
+//  Card.swift
+//  FlashCardsApp
+//
+//  Created by Mateo Elosua on 2021-12-07.
+//
+
+import Foundation
+struct card {
+    let question: String
+    let answer: String
+}
+
+let listOfCards = {
+    card(question: "What's\"powerHouse\" of a cell?",
+         answer: "mitocondria")
+}
+
+    card(question: "What is the largest bone in your body?",
+     answer: "femur")
+
+card(question: "What is acceleration due to gravity?",
+     answer: "9/80 m/s squared")
